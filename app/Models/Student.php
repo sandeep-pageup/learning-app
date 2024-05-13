bd2dbfed8b3ebc81451552f5bfb46a38c0b5c003
sandeep patel <spatel@pageupsoft.com>
2024-05-13 18:36:25 +0530

    Implemented Db seeding with factory

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,10 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
-
-    public function posts() {
-        return collect(explode(',', $this->time_table_group_ids))->map(function ($time_table_group_id) {
-            return Post::find($time_table_group_id);
-        });
-    }
+    public $timestamps = false;
 } 
