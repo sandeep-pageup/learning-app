61e6c89d20050855344887ed5c35dc245b988cc1
sandeep patel <spatel@pageupsoft.com>
2024-05-29 12:55:37 +0530

    Implemented Basic Collection.

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,39 @@ use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('all-users' , function() {
+    foreach(User::all() as $user) {
+        echo $user->name." , ";
+    }
+});
 
-Route::get('students', function() {
-    $student = Student::where('id', 1)->first();
-    return $student->posts();
+Route::get('all-users-map' , function() {
+    return User::all()->reject(function(User $user) {
+        return $user->id == 1;
+    })->map(function(User $user) {
+        return $user->name;
+    });
+});
+
+Route::get('collect' , function (){
+    $collection = collect(['sandeep' , 'patel']);
+    return $collection;
+});
+
+Route::get('contains' , function() {
+    $users = User::all();
+    return $users->contains(14); //passing primary key.
+});
+
+Route::get('diff' , function() {
+    $users = User::all();
+    return $users->diff(User::whereIn('id' , [1,2,3,4,5])->get());
+});
+
+Route::get('except' , function(){
+    return User::all()->except([1,2]);
+});
+
+Route::get('find' , function() {
+    return User::all()->find([1,2,3]);
 });
