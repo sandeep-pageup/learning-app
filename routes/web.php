e90a88b3b90af4891b82b9f535f2b7c54b87dd1f
sandeep patel <spatel@pageupsoft.com>
2024-06-10 17:04:32 +0530

    Pagination

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,8 @@
 
 use App\Models\Student;
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('students', function() {
-    $student = Student::where('id', 1)->first();
-    return $student->posts();
+Route::get('users', function() {
+    return view('users' , ['users' => User::paginate(5)]);
 });
