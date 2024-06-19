d201e3c6b8642e9d9e82ec8d2223423436108ac3
sandeep patel <spatel@pageupsoft.com>
2024-06-19 10:52:02 +0530

    Implemented File Storage

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,8 @@
 <?php
 
-use App\Models\Student;
-use App\Models\User;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('students', function() {
-    $student = Student::where('id', 1)->first();
-    return $student->posts();
+Route::get('store', function() {
+    Storage::disk('local')->put('demo1.txt' , 'Sandeep Patel');
 });
