7585e4de3e3ef679162af57a1fb2889125969010
sandeep patel <spatel@pageupsoft.com>
2024-06-28 16:37:58 +0530

    Implemented Elastic Search

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,9 @@
 <?php
 
-use App\Models\Student;
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
-Route::get('students', function() {
-    $student = Student::where('id', 1)->first();
-    return $student->posts();
+Route::get('users', function () {
+    return User::search('y')->get();
 });
