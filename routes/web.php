7f8a3d2b18065f57252fc25a1657b980ecae9cc9
sandeep patel <spatel@pageupsoft.com>
2024-07-04 15:53:14 +0530

    Implemented Select2

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,4 @@ use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('students', function() {
-    $student = Student::where('id', 1)->first();
-    return $student->posts();
-});
+Route::view('/' , 'welcome');
